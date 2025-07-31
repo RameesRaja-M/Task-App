d4d9458c734cffc1cc5618fe4af94d7fb216b846
Ramees Raja <rameesraja27082002@gmail.com>
2025-07-31 16:45:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const { default: mongoose } = require('mongoose');
 // Create a Express Instence 
 const app = express();
 app.use(cors({
-    origin:process.env.FRONTEND_URL || 'https://task-app-plum-seven.vercel.app/',
+    origin:process.env.FRONTEND_URL || 'https://task-app-plum-seven.vercel.app',
     methods: 'GET,PUT,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
